1861bad1683621badfe6813626e0911280e35901
frank paulsen <frobnic+ita@gmail.com>
2019-08-08 12:23:43 +0200

    code inserted

--- 0601A2Felderrechnen/Program.cs
+++ 0601A2Felderrechnen/Program.cs
@@ -10,6 +10,29 @@ namespace _0601A2Felderrechnen
     {
         static void Main(string[] args)
         {
+            sbyte[] feld = new sbyte[10];
+            Random zuf = new Random();
+            int sum=0;
+            sbyte min=127, max=0, avg=0;
+
+            //        geht nicht, weil das foreach-element kein lvalue ist!
+            //          foreach (sbyte f in feld)
+            //              f = (sbyte)(zuf.Next(1,33));
+
+            for (int i = 0; i < 10; i++)
+            {
+                feld[i] = (sbyte)(zuf.Next(1, 33));
+            }
+
+            foreach (var f in feld)
+            {
+                min = Math.Min(min, f);
+                max = Math.Max(max, f);
+                sum += f;
+            }
+            Console.WriteLine("Min={0} Max={1} Sum={2} Avg={3}", min, max, sum, sum / 10);
+
+
         }
     }
 }
